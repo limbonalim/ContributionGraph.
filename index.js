ff5822d61d09a921a5856e184c3f7910b5b95d1b
limbonalim <zeterfrost@gmail.com>
2023-11-28 11:00:15 +0600

    added getResponse and getCell func

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const container = document.getElementById('root');
 const BASE_URL = 'https://dpg.gg/test/calendar.json';
+
+const getResponse = async () => {
+    try {
+        const response = await fetch(BASE_URL);
+        if (response.ok) {
+            const data = await response.json();
+            return data
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+const getCell = () => {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    container.appendChild(cell);
+};
+
