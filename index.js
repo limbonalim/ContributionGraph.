38c77d256c5dd31a9df6210e8017fbe1706c868f
limbonalim <zeterfrost@gmail.com>
2023-11-28 15:29:12 +0600

    created getToolTipTitle func

--- index.js
+++ index.js
@@ -38,19 +38,42 @@ const setContribution = (contribution, date, day) => {
             } else if (value > 30) {
                 day.classList.add('extraHight');
             }
+            day.setAttribute('title', getToolTipTitle(date, value));
         }
     }
+};
+
+const getToolTipTitle = (date, contributionValue) => {
+    const formatDate = (date) => {
+        const currentDay = new Date(date);
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        const dayOfWeek = daysOfWeek[currentDay.getDay()];
+        const month = months[currentDay.getMonth()];
+        const dayOfMonth = currentDay.getDate();
+        const year = currentDay.getFullYear();
+
+        const formattedDate = `${dayOfWeek} ${month} ${dayOfMonth}, ${year}`;
+        return formattedDate;
+    };
+
+    if (!contributionValue) {
+        return `${formatDate(date)} No contribution`;
+    }
+
+    return `${formatDate(date)} ${contributionValue} contributions`;
+    
 };
 
 const run = async () => {
     const contribution = await getResponse();
     const today = new Date();
     const daysInPast = 357;
-    
     const weeks = [];
     let test = [];
 
-    for (let i = 0; i < daysInPast; i++) {
+    for (let i = 1; i <= daysInPast; i++) {
         const day = getCell();
         day.setAttribute('data-bs-toggle', 'tooltip');
         day.setAttribute('data-bs-placement', 'top');
@@ -58,8 +81,9 @@ const run = async () => {
         const currentDay = new Date(today);
         currentDay.setDate(today.getDate() - (daysInPast - i));
         const dateDay = formattDate(currentDay);
-        day.setAttribute('title', currentDay);
+        day.setAttribute('title', getToolTipTitle(dateDay, 0))
         setContribution(contribution, dateDay, day);
+
         test.push(day)
         if (test.length === 7) {
             weeks.push(test);
@@ -68,6 +92,7 @@ const run = async () => {
     }
     weeks.forEach((week) => {
         const weekWrapper = document.createElement('div');
+        weekWrapper.classList.add('week')
         week.forEach(day => {
             weekWrapper.append(day)
         })
