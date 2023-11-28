f57867514ac5e7dbb8d9e4ffa2aa78451a73e9ba
limbonalim <zeterfrost@gmail.com>
2023-11-28 12:34:02 +0600

    updated run func and created formattDate func

--- index.js
+++ index.js
@@ -13,23 +13,42 @@ const getResponse = async () => {
     }
 };
 
-
+const formattDate = (date) => {
+    const newDate = new Date(date);
+    const formattedDate = newDate.toISOString().slice(0, 10);
+    return formattedDate
+};
 
 const getCell = () => {
     const cell = document.createElement('div');
     cell.className = 'cell';
-    container.appendChild(cell);
+    return cell
 };
 
 const run = async () => {
     const contribution = await getResponse();
     const today = new Date();
     const daysInPast = 357;
-    console.log(container)
     for (let i = 0; i < daysInPast; i++) {
         const day = getCell();
         const currentDay = new Date(today);
         currentDay.setDate(today.getDate() - (daysInPast - i));
+        const dateDay = formattDate(currentDay);
+        for (let data in contribution) {
+            if (data === dateDay) {
+                const value = contribution[data];
+                if (value > 0 && value <= 9) {
+                    day.classList.add('low');
+                } else if (value >= 10 && value <= 19) {
+                    day.classList.add('moreThenLow');
+                } else if (value >= 20 && value <= 29) {
+                    day.classList.add('hight');
+                } else if (value > 30) {
+                    day.classList.add('extraHight');
+                }
+            }
+        }
+        container.appendChild(day);
     }
 }
 
